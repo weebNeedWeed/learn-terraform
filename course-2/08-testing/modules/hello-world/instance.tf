65881e0fd5b6898169557ae98f2bd5f0a3caf2ce
weebNeedWeed <rivenmle@gmail.com>
2025-03-24 23:17:13 +0700

    ab

--- course-2/08-testing/modules/hello-world/instance.tf
+++ course-2/08-testing/modules/hello-world/instance.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
 resource "aws_instance" "instance" {
   ami                    = "ami-0b03299ddb99998e9"
   instance_type          = "t2.micro"
